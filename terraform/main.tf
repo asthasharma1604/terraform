22f2ac1dc5a01e7b168a08b301bd3bb70879f053
asthasharma1604 <sharmaasu167@gmail.com>
2026-09-08 07:31:14 +0000

    Testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,6 @@
 resource "local_file" "example" {
-  filename = "${path.module}/example.txt"
-  content  = "Hello! from Terraform"
+  filename             = "${path.module}/${var.environment}-credentials.txt"
+  content              = "admin_password=test-only-hardcoded-password"
+  file_permission      = "0777"
+  directory_permission = "0777"
 }
